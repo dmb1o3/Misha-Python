f4fe0de96c7da7f0c4dcedb0a131ee5166fa5d94
Raspix <126830126+Raspix13@users.noreply.github.com>
2025-03-26 14:09:22 -0400

    depth testing methods

--- Stereo/MSDTest.py
+++ Stereo/MSDTest.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 from rps import RPS
 from calibrate_light import calibrate_light_test
 
+from MyCalcs import (
+    runTest
+)
 import argparse
 import numpy as np
 import time
@@ -13,6 +16,8 @@ from matplotlib import pyplot as plt
 from skimage import io, transform
 import cv2 as cv
 from PIL import Image
+from scipy.sparse import lil_matrix
+from scipy.ndimage import convolve
 
 # Choose a method
 METHOD = RPS.L2_SOLVER    # Least-squares
@@ -25,11 +30,16 @@ DATA_FOLDERNAME = './data/target/'    # Lambertian diffuse with cast shadow
 MASK_FOLDERNAME = './data/mask/'
 LIGHT_FOLDERNAME = './data/light/'
 RESULTS_FOLDERNAME = './data/results/'
-IMAGES_NAME = 'target_'
-NORMAL_FILENAME = './data/results/normalmap_purple.png' # './data/normal_run/normal_mapping_a.png' target_normal.png
+IMAGES_NAME = 'innard_'
+NORMAL_FILENAME = './data/results/monkey_normal.png' #'./data/results/innard_normal.jpg' # './data/normal_run/normal_mapping_a.png' target_normal.png
 EXPORT_FILENAME = './data/results/export_normal.png'
+EXPORT_FILENAME2 = './data/results/export_normal2.png'
+EXPORT_FILENAME3 = './data/results/export_normal3.png'
+EXPORT_FILENAME4 = './data/results/export_normal4.png'
 
-scale = 0.5 # Makes image smaller to improve performance for testing, should be 1 for actual runs
+HEIGHT_FILENAME = './data/results/export_normal4.png'
+
+scale = 1.0 # Makes image smaller to improve performance for testing, should be 1 for actual runs
 
 showImages = True
 renderModel = True
@@ -78,7 +88,7 @@ def runPStereo(rps):
     #rps.save_normalmap_as_png(RESULTS_FOLDERNAME + IMAGES_NAME + 'normal.png')
     
     
-
+# For seeing the 3d model of a height map
 def runHeightGen(rps):
     NORMAL_MAP_A_IMAGE: np.ndarray = np.load("./downscaledTargetNormals.npy")
     start = time.time()
@@ -121,6 +131,18 @@ def runHeightGen(rps):
         plt.waitforbuttonpress
 
 
+
+def runHeightModel():
+    heights: np.ndarray = cv.imread(HEIGHT_FILENAME)
+
+    
+    x, y = np.meshgrid(range(heights.shape[1]), range(heights.shape[0]))
+    _, axes = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
+    _ = axes.scatter(x, y, heights[:, :, 0], c=heights[:, :, 0])
+
+   # cv.imwrite(EXPORT_FILENAME, (heights *255).astype(np.uint8))
+
+    plt.show()
 
 
 def runHeightGen2():
@@ -272,37 +294,213 @@ def compute_height_map():
 
     plt.show()
 
-def compute_my_heights():
+def compute_height_map2():
     NORMAL_MAP_A_IMAGE_FIRST: np.ndarray = io.imread(NORMAL_FILENAME)
-    NORMAL_MAP_A_IMAGE = transform.resize(NORMAL_MAP_A_IMAGE_FIRST, (int(NORMAL_MAP_A_IMAGE_FIRST.shape[0] * scale), int(NORMAL_MAP_A_IMAGE_FIRST.shape[1] * scale)), anti_aliasing=True)
-    NORMAL_MAP_A_IMAGE = (NORMAL_MAP_A_IMAGE).astype(int)
+    NORMAL_MAP_A_IMAGE = transform.resize(NORMAL_MAP_A_IMAGE_FIRST, (int(NORMAL_MAP_A_IMAGE_FIRST.shape[0] * scale), int(NORMAL_MAP_A_IMAGE_FIRST.shape[1] * scale)),
+                                 anti_aliasing=True)
+    NORMAL_MAP_A_IMAGE = (NORMAL_MAP_A_IMAGE * 255).astype(int)
     map = NORMAL_MAP_A_IMAGE
-    height_map = np.zeros((map.shape[0], map.shape[1]))
-    for row in range(map.shape[0]):  # Iterate over rows
-        row_val = 0
-        for col in range(map.shape[1]):
-            row_val += map[row][col][0]/2.0
-            height_map[row][col] += row_val
-        print(map[row : 0])
-        print(height_map[row])
 
+    maskImage = io.imread(MASK_FOLDERNAME + IMAGES_NAME + 'mask.png')
+    #print(maskImage.shape)
+    maskImage = transform.resize(maskImage, (int(maskImage.shape[0] * scale), int(maskImage.shape[1] * scale)),
+                                 anti_aliasing=True)
+    maskImage = (maskImage * 255).astype(int)
+    maskImage = cv.convertScaleAbs(maskImage)
+
+
+    print(map.shape)
+    print(maskImage.shape)
+    maskImage = cv.cvtColor(maskImage, cv.COLOR_RGB2GRAY)
+
+    heights = normal_to_height_kernel(NORMAL_MAP_A_IMAGE)
+    val =  np.max(heights) - np.min(heights)
+    heights = (heights-np.min(heights))/(np.max(heights)-np.min(heights))
+    heights = (heights * val)
+
+    #figure, axes = plt.subplots(1, 2, figsize=(7, 3))
+    #_ = axes[0].imshow(NORMAL_MAP_A_IMAGE)
+    #_ = axes[1].imshow(heights)
+
+    #x, y = np.meshgrid(range(heights.shape[1]), range(heights.shape[0]))
+    #_, axes = plt.subplots(1, 1, subplot_kw={"projection": "3d"})
+    #_ = axes.scatter(x, y, heights, c=heights)
+
+    cv.imwrite(EXPORT_FILENAME, (heights *255).astype(np.uint8))
+
+    #plt.show()
+
+
+def normal_to_height_kernel(normal_map):
+    # Design kernels to approximate partial derivatives in X, Y, and Z directions
+    kernel_x = np.array([[0, 0, 0], [-1, 0, 1], [0, 0, 0]])
+    kernel_y = np.array([[0, -1, 0], [0, 0, 0], [0, 1, 0]])
+    kernel_z = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1]])
+
+    # Perform convolutions to extract height information in X, Y, and Z directions
+    height_x = convolve(normal_map[:, :, 0], kernel_x)
+    height_y = convolve(normal_map[:, :, 1], kernel_y)
+    height_z = convolve(normal_map[:, :, 2], kernel_z)
+
+    convx = (height_x-np.min(height_x))/(np.max(height_x)-np.min(height_x))
+    convy = (height_y-np.min(height_y))/(np.max(height_y)-np.min(height_y))
+    convz = (height_z-np.min(height_z))/(np.max(height_z)-np.min(height_z))
+
+    cv.imwrite(EXPORT_FILENAME2, (convx *255).astype(np.uint8))
+    cv.imwrite(EXPORT_FILENAME3, (convy *255).astype(np.uint8))
+    cv.imwrite(EXPORT_FILENAME4, (convz *255).astype(np.uint8))
+
+
+    # Combine the results to get the height map
+    height_map = (height_x + height_z + height_z)/3
+
+    return height_map
+
+
+def normal_to_height_integration(normal_map):
+    height_map = np.zeros(normal_map.shape[:2])
+
+    for i in range(normal_map.shape[0]):
+        for j in range(normal_map.shape[1]):
+            # Integrate the normal map to get the height map
+            height_map[i, j] = np.sum(normal_map[:i+1, :j+1, 2])
+
+    cv.imwrite(EXPORT_FILENAME, (height_map *255).astype(np.uint8))
+    return height_map
+
+def compute_my_heights():
+    normal_map: np.ndarray = io.imread(NORMAL_FILENAME)
+    #print(normal_map.shape)
+    #print(normal_map[0])
+    normal_map = transform.resize(normal_map, (int(normal_map.shape[0] * scale), int(normal_map.shape[1] * scale)), anti_aliasing=True)
+    normal_map = (normal_map * 255).astype(int)
+    height_map = np.ones((normal_map.shape[0], normal_map.shape[1])) * 127
+    #print(normal_map[0])
+    for row in range(normal_map.shape[0]):  # Iterate over rows
+        row_val = 0
+        for col in range(normal_map.shape[1]):
+            row_val += normal_map[row][col][0]/2.0 - 64
+            height_map[row][col] += row_val
+        #print(map[row])
+        #print(height_map[row])
+    print(height_map[0])
     height_map = (height_map-np.min(height_map))/(np.max(height_map)-np.min(height_map))
+    print(height_map[20])
     cv.imwrite(EXPORT_FILENAME, (height_map *255).astype(np.uint8))
 
+    height_map2 = np.ones((normal_map.shape[0], normal_map.shape[1])) * 127
+    for col in range(normal_map.shape[1]):  # Iterate over rows
+        col_val = 0
+        for row in range(normal_map.shape[0]):
+            col_val += normal_map[row][col][1]/2.0 - 64
+            height_map2[row][col] += col_val
+    height_map2 = (height_map2-np.min(height_map2))/(np.max(height_map2)-np.min(height_map2))
+    cv.imwrite(EXPORT_FILENAME2, (height_map2 *255).astype(np.uint8))
+
+    height_map3 = (height_map + height_map2)/2
+    cv.imwrite(EXPORT_FILENAME3, (height_map3 *255).astype(np.uint8))
+
+def normal_to_height_fourier(normal_map):
+    # Perform Fourier Transform on the normal map
+    f = np.fft.fft2(normal_map[:,:,0])  # Assuming normal_map[:,:,0] contains the X component of normal vectors
+    fshift = np.fft.fftshift(f)
+
+    # Calculate the magnitude spectrum
+    magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+    # Inverse Fourier Transform to get the height map
+    height_map = np.fft.ifft2(fshift)
+    height_map = np.abs(height_map)
+
+    return height_map
+
+def normal_to_height_fourier2(normal_map):
+    height_map = np.zeros(normal_map.shape[:2])
+
+    for i in range(3):  # Process each component separately (X, Y, Z)
+        f = np.fft.fft2(normal_map[:,:,i])
+        fshift = np.fft.fftshift(f)
+
+        # Inverse Fourier Transform to get the height map for this component
+        height_component = np.fft.ifft2(fshift)
+        height_map += np.abs(height_component)  # Accumulate height information from all components
+
+    return height_map
+
+def DepthMap(surfNormals, maskImage):
+    z = []
+
+    print(surfNormals.shape)
+    print(maskImage.shape)
+
+    #maskImage = cv.cvtColor(maskImage, cv.COLOR_BGR2GRAY)
+
+    nrows, ncols = maskImage.shape
+    objectPixelRow, objectPixelCol = np.where(maskImage)
+    objectPixels = np.column_stack((objectPixelRow, objectPixelCol))
+
+    index = np.zeros((nrows, ncols))
+    numPixels = objectPixels.shape[0]
+
+    M = lil_matrix((2*numPixels, numPixels))
+    b = np.zeros((2*numPixels, 1))
+
+    for d in range(numPixels):
+        pRow = objectPixels[d, 0]
+        pCol = objectPixels[d, 1]
+        nx = surfNormals[pRow, pCol, 0]
+        ny = surfNormals[pRow, pCol, 1]
+        nz = surfNormals[pRow, pCol, 2]
+
+        if (index[pRow, pCol+1] > 0) and (index[pRow-1, pCol] > 0):
+            M[2*d, index[pRow, pCol]] = 1
+            M[2*d, index[pRow, pCol+1]] = -1
+            b[2*d] = nx / nz
+
+            M[2*d+1, index[pRow, pCol]] = 1
+            M[2*d+1, index[pRow-1, pCol]] = -1
+            b[2*d+1] = ny / nz
+
+        # Other if-else conditions should be similarly translated
+
+    x = np.linalg.lstsq(M.tocsr(), b, rcond=None)[0]
+    x = x - np.min(x)
+
+    tempShape = np.zeros((nrows, ncols))
+    for d in range(numPixels):
+        pRow = objectPixels[d, 0]
+        pCol = objectPixels[d, 1]
+        tempShape[pRow, pCol] = x[d]
+
+    z = np.zeros((nrows, ncols))
+    for i in range(nrows):
+        for j in range(ncols):
+            z[i, j] = tempShape[nrows-i-1, j]
+
+    return z
 
 
 
 def main(args):
     print("Running")
-    #calibrate_light_test(DATA_FOLDERNAME, MASK_FOLDERNAME, LIGHT_FOLDERNAME, 4, IMAGES_NAME, False)
+
+    # Run this first with calibration
+    #calibrate_light_test(DATA_FOLDERNAME, MASK_FOLDERNAME, LIGHT_FOLDERNAME, 4, IMAGES_NAME, False) 
     rps = RPS()
+    # Run this with the light file from the last to get normal map
     #runPStereo(rps)
+
+
     #runHeightGen(rps)
     #runHeightGen2()
     #compute_height_map()
     #compute_normal_map(rps)
-    compute_height_map()
+    #compute_height_map2()
     #compute_my_heights()
+
+    runTest()
+    
+    #runHeightModel()
 
 
 if __name__ == '__main__':
